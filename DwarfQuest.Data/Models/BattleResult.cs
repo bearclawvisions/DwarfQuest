d52f1e433e8dfc731f303e3283946826635074f3
bearclawvisions <t.d.schoolderman@gmail.com>
2025-11-23 13:13:01 +0100

    basic setup for battleresults

--- DwarfQuest.Data/Models/BattleResult.cs
+++ DwarfQuest.Data/Models/BattleResult.cs
@@ -3,6 +3,7 @@ namespace DwarfQuest.Data.Models;
 public class BattleResult
 {
     public int Experience { get; set; }
+    public int SkillPoints { get; set; }
     public int Money { get; set; }
     public string Message { get; set; } = string.Empty;
     // todo Items
--- DwarfQuest/Scripts/CombatResults.cs
+++ DwarfQuest/Scripts/CombatResults.cs
@@ -10,11 +10,21 @@ public partial class CombatResults : Control
 	private readonly CombatService _combatService = GameManager.CombatService;
 	private BattleResult _result;
 	
+	private Label _expAmount;
+	private Label _skillPointsAmount;
+	private Label _moneyAmount;
+	
 	public override void _Ready()
 	{
+		_expAmount = GetNode<Label>("%ExpAmount");
+		_skillPointsAmount = GetNode<Label>("%SkillPointsAmount");
+		_moneyAmount = GetNode<Label>("%MoneyAmount");
+		
 		_result = _combatService.GetBattleResult();
 		
-		GD.Print($"exp: {_result.Experience} - money: {_result.Money} - message: {_result.Message}");
+		_expAmount.Text = _result.Experience.ToString();
+		_skillPointsAmount.Text = "2";
+		_moneyAmount.Text = _result.Money.ToString();
 	}
 	
 	// use input to close and go to overworld
